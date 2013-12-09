c70a76bc9440b964469d2b940be6158a88977384
Greg A Morrissey <greg-morrissey@kellogg.northwestern.edu>
2013-12-09 05:01:23 -0600

    Working on Posts

--- db/migrate/20131125060352_create_posts.rb
+++ db/migrate/20131125060352_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.string :post_title
       t.integer :user_id
       t.boolean :published
       t.date :published_on
